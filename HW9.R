3db2ad80b2373b3864b1c199f4c23bbe52bcf379
yuliia-heleveria <148816812+yuliia-heleveria@users.noreply.github.com>
2025-03-30 22:36:33 -0400

    T

---
+++ HW9.R
@@ -0,0 +1,8 @@
+
+################################################################################
+# Question 1a:  MLEs using a Gamma distribution
+################################################################################
+#Function to compute Maximum Likelihood
+MLE.gamma <- function(par, data){
+  
+}
