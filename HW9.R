aa19d58c86891dba945630b49ea939e8f3bf8bf0
yuliia-heleveria <148816812+yuliia-heleveria@users.noreply.github.com>
2025-04-01 10:42:56 -0400

    Created a plot and exponentiated calculations

--- HW9.R
+++ HW9.R
@@ -1,5 +1,4 @@
-#am i computing and interpreting Q right
-#do i need to graph anything
+#why do i get that lognormal is the best
 
 ################################################################################
 # HW 9 R CODE
@@ -86,7 +85,7 @@ Gamma.loglik <- llgamma(par = c(alpha.MLE, beta.MLE), data = dat.precip.long$Pre
 
 #compute the likelihood ratio
 Weibull.loglik <- 2166.496
-q.gamma.weibull <- Weibull.loglik/Gamma.loglik
+q.gamma.weibull <- exp(Weibull.loglik-Gamma.loglik)
 
 ################################################################################
 # Question 1d: Compare the Weibull and the Log-Normal distribution
@@ -95,10 +94,60 @@ q.gamma.weibull <- Weibull.loglik/Gamma.loglik
 Lognorm.loglik <- lllognorm(c(mu.MLE, sigma.MLE), data = dat.precip.long$Precipitation, neg = T)
 
 #compute the likelihood ratio
-q.lognorm.webull <- Weibull.loglik/Lognorm.loglik
+q.lognorm.webull <- exp(Weibull.loglik-Lognorm.loglik)
 
 ################################################################################
 # Question 1e: Compare the Gamma and the Log-Normal distribution
 ################################################################################
 #compute the likelihood ratio
-q.gamma.lognorm <- Gamma.loglik/Lognorm.loglik
+q.gamma.lognorm <- exp(Gamma.loglik-Lognorm.loglik)
+
+################################################################################
+# Make a plot
+################################################################################
+#Get data for Weibull
+llweibull <- function(par, data, neg=F){
+  # a <- par[1]
+  # sigma <- par[2]
+  a <- exp(par[1]) # go from (-inf,inf) to (0,inf)
+  sigma <- exp(par[2]) # go from (-inf,inf) to (0,inf)
+  
+  ll <- sum(log(dweibull(x=data, shape=a, scale=sigma)), na.rm=T)
+  
+  return(ifelse(neg, -ll, ll))
+}
+
+MLEs <- optim(fn = llweibull,
+              par = c(1,1),
+              data = dat.precip.long$Precipitation,
+              neg=T)
+
+(MLEs$par <- exp(MLEs$par)) # transform
+
+#Data to plot
+ggdat.weibull <- tibble(x = seq(0,15,length.out=1000)) |>
+  mutate(pdf.mle = dweibull(x=x, shape=MLEs$par[1], scale=MLEs$par[2]))
+
+ggdat.gamma <- tibble(x = seq(0,15,length.out=1000)) |>
+  mutate(pdf.mle = dgamma(x=x, shape=alpha.MLE, rate=beta.MLE))
+
+ggdat.lognorm <- tibble(x = seq(0,15,length.out=1000)) |>
+  mutate(pdf.mle = dlnorm(x=x, meanlog =mu.MLE, sdlog = sigma.MLE))
+
+#Make a histogram with MLEs of three distributions superimposed
+ggplot() +
+  geom_histogram(data=dat.precip.long,
+                 aes(x=Precipitation, y=after_stat(density)),
+                 breaks=seq(0, 15, 1),
+                 color="lightgrey")+
+  geom_line(data=ggdat.weibull,
+            aes(x=x, y=pdf.mle, color="Weibull"))+
+  geom_line(data=ggdat.gamma,
+            aes(x=x, y=pdf.mle, color="Gamma"))+
+  geom_line(data=ggdat.lognorm,
+            aes(x=x, y=pdf.mle, color="Log-Normal"))+
+  geom_hline(yintercept = 0)+
+  theme_bw()+
+  xlab("Precipitation (Inches)")+
+  ylab("Density")+
+  labs(color="")
